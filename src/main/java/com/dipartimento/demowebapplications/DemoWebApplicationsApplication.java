db4ca16cc41a43f6acf08514abcfa39ddca5ff8e
kristian <reale@dlvsystem.com>
2024-10-25 10:12:03 +0200

    html, css, javascript

---
+++ src/main/java/com/dipartimento/demowebapplications/DemoWebApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.dipartimento.demowebapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebApplicationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebApplicationsApplication.class, args);
+    }
+
+}
