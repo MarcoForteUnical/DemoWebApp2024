73ff1df915998cdf08941f2cc671e7d639f96441
kristian <reale@dlvsystem.com>
2024-10-30 17:50:31 +0100

    Preparazione Servlet e Thymeleaf

--- src/main/java/com/dipartimento/demowebapplications/DemoWebApplicationsApplication.java
+++ src/main/java/com/dipartimento/demowebapplications/DemoWebApplicationsApplication.java
@@ -2,8 +2,10 @@ package com.dipartimento.demowebapplications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class DemoWebApplicationsApplication {
 
     public static void main(String[] args) {
